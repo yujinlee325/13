3dc5c4b906388f349b321bbb0932e2cd796bd24f
yujinlee325 <yu_jin325@naver.com>
2020-11-26 16:27:44 +0900

    1126_03

--- practice/practice/main.c
+++ practice/practice/main.c
@@ -8,10 +8,53 @@
 
 #include <stdio.h>
 
+int is_whitespace(char c){
+
+    if (c == ' ' ||
+        c == '\n' ||
+        c == '\r' ||
+        c == '\t' ||
+        c == '(' )
+        return 1;
+        
+    return 0;
+}
+
+int fget_word(FILE *fp, char* word){
+   
+    char c;
+    int cnt;
+    
+    while((c=fgetc(fp)) != EOF) {
+        if(is_whitespace(c) == 0)
+            break;
+    }
+    if (c == EOF)
+        return 0;
+    
+    //filling out the word
+    cnt = 0;
+    word[cnt++] = c;
+    word[cnt] = '\0' ;
+    
+    while ((word[cnt]=fgetc(fp)) != EOF){
+        if(is_whitespace(word[cnt]) == 1)
+        {
+            word[cnt] = '\0';
+            break;
+        }
+        cnt++;
+    }
+    return cnt;
+}
+
 
 int main(void){
     FILE* fp ;
     char filename[100];
+    char word[100];
+    
+    int cnt;
     
     //file open
     printf("input the file name : ");
@@ -23,9 +66,16 @@ int main(void){
         return -1 ;
     }
     
-    //bring words from fp
+    cnt = fget_word(fp, word);
+    printf("%s (%i)\n", word, cnt);
     
-    //bring a word
+    cnt = fget_word(fp, word);
+    printf("%s (%i)\n", word, cnt);
     
-    fclose(filename);
+    cnt = fget_word(fp, word);
+    printf("%s (%i)\n", word, cnt);
+    
+   
+    
+    fclose(fp);
 }
